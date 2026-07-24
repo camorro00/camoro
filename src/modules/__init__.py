20eafd4a3726c1cc49e97655710e441c232232b2
CamarO-Pro <fggyeyyyejchsysyfgd@gmail.com>
2026-07-24 02:56:15 +0200

    Update __init__.py

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -1,6 +1,2 @@
-from .osint_scanner import OSINTScanner
-from .password_engine import PasswordEngine
-from .ai_password_engine import AIPasswordEngine
-from .login_tester import LoginTester
-from .proxy_manager import ProxyManager
-from .report_generator import ReportGenerator
+# src/__init__.py
+__version__ = "3.0.0"
